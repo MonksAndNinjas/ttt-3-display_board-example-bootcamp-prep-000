3922a87d9918f4a411f01c53805104ae93230e48
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-08 20:30:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-cell = "   "
-  
-divider = "-----------"
-  
-row = "#{cell}|#{cell}|#{cell}"
-
 def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+end
+
+def display_board
+  puts "   |   |   
+  -----------"
 end
